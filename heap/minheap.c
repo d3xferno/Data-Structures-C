44d788956cac8aed035670ea99f13315b1dbb275
Shajith Hameed <shaham@pop-os.localdomain>
2021-11-29 16:42:18 +0530

    stuff added

--- heap/minheap.c
+++ heap/minheap.c
@@ -2,48 +2,86 @@
 #include<stdlib.h>
 
 typedef struct heap{
-    int data[101];
+    char emp[101];
+    int sal[101];
     int cur,sz;
 }heap;
 
 void init(heap *h,int size){
-    h->data[0]=-1;
+    h->sal[0]=-1;
+    h->emp[0] = '#';
     h->cur = 0;
     h->sz = size;
 }
 
-void insert(heap *h,int x){
-    h->data[++(h->cur)] = x;
+void insert(heap *h,char y,int x){
+    h->sal[++(h->cur)] = x;
+    h->emp[h->cur] = y;
     int i=h->cur;
-    while(i>0 && h->data[i/2]>h->data[i]){
+    while(i>0 && h->sal[i/2]>h->sal[i]){
         int tmp;
-        tmp = h->data[i/2];
-        h->data[i/2] = h->data[i];
-        h->data[i] = tmp;
+        tmp = h->sal[i/2];
+        h->sal[i/2] = h->sal[i];
+        h->sal[i] = tmp;
+        char temp;
+        temp = h->emp[i/2];
+        h->emp[i/2] = h->emp[i];
+        h->emp[i] = temp;
         i/=2;
     }
 }
 
 void delete(heap *h){
-    int ans = h->data[1];
-    int last = h->data[h->cur];
+    int ans = h->sal[1];
+    char answer = h->emp[1];
+    int last = h->sal[h->cur];
+    char lt = h->emp[h->cur];
     h->cur--;
-    h->data[1] = last;
-    int l=2,r=3;
-    printf("\n%d",ans);
+    //h->sal[1] = last;
+    int ch,p;
+    for(p=1;2*p<=h->cur;p=ch){
+        ch = 2*p;
+        if(ch!=h->cur && h->sal[ch]>h->sal[ch+1]){
+            ch++;
+        }
+        if(last>h->sal[ch]){
+            h->sal[p] = h->sal[ch];
+            h->emp[p] = h->emp[ch];
+        }else break;
+    }
+    h->sal[p] = last;
+    h->emp[p] = lt;
+    printf("(%c,%d)",answer,ans);
 }
 
 void display(heap *h){
     for(int i=1;i<=h->cur;i++){
-        printf("%d ",h->data[i]);
+        printf("(%c,%d)\n",h->emp[i],h->sal[i]);
+    }
+}
+
+void disdelorder(heap *h){
+    while(h->cur!=0){
+        delete(h);
+        printf("\n");
     }
 }
 
 int main(){
     heap *h = (heap *)malloc(sizeof(heap));
-    insert(h,1);
-    insert(h,2);
-    insert(h,3);
+    init(h,100);
+    printf("Employee Insertion Order:\n");
+    insert(h,'A',15000);
+    insert(h,'K',12000);
+    insert(h,'R',4000);
+    insert(h,'T',3500);
+    insert(h,'L',4600);
+    insert(h,'P',6000);
+    insert(h,'Y',8600);
+    display(h);
+    printf("\nEmployees are removed in the order:\n");
+    disdelorder(h);
+    printf("\n");
     display(h);
     return 0;
 }
